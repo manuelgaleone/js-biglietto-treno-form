bb83fae881dee1fdfbec4bcfd451fe82db064750
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-07 17:17:31 +0200

    Fixed price problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,44 @@
-//Dati
-let prezzoChilometri = 0.21
-
-//Richiesta d'età
-let nomePasseggero = document.getElementById("nome").value;
-let etaPasseggero = Number(document.getElementById("eta")).value;
-let chilometri = Number(document.getElementById("chilometri")).value;
-
-//Calcolo prezzo
-let prezzo = chilometri * prezzoChilometri
-let costoFinale
-
-//Calcolo sconto minorenni 20%
-if (etaPasseggero < 18) {
-
-    costoFinale = prezzo - (prezzo * 20 / 100)     
-
-} else if (etaPasseggero > 65) {
-
-    costoFinale = prezzo - (prezzo * 40 / 100)  
-
-} else {
-
-    costoFinale = prezzo
-    
-}
-
-//Funzione Click
+//Funzione Click Genera e Comunicazione
 const generaButton = document.getElementById("genera");
 generaButton.addEventListener("click", function(){
 
+    //Dati
+    const prezzoChilometri = 0.21
+    const select = document.getElementById('etaSelect');
+    const option = select.options[select.selectedIndex];
+
+    //Richiesta Dati Passeggero
+    const nomePasseggero = document.getElementById("nome").value;
+    const chilometri = document.getElementById("chilometri").value;
+
+    //Calcolo prezzo
+    const prezzo = chilometri * prezzoChilometri
+    let costoFinale
+
+    //Calcolo Sconti
+    if (option.value == "minorenne") {
+
+        costoFinale = prezzo - (prezzo * 20 / 100)     
+
+    } else if (option.value === "over") {
+
+        costoFinale = prezzo - (prezzo * 40 / 100)  
+
+    } else if (option.value === "standard") {
+
+        costoFinale = prezzo
+    }
+
     document.getElementById("ticket").style.display = "block";
     document.getElementById("nomeTicket").innerHTML = nomePasseggero;
-
+    document.getElementById('offerta').innerHTML = option.text;
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("codice").innerHTML = Math.floor(Math.random() * 5000);
+    document.getElementById("costoBiglietto").innerHTML = costoFinale;
 
 })
 
+//Funzione Click Annulla
 const annullaButton = document.getElementById("annulla");
 annullaButton.addEventListener("click", function(){
 
